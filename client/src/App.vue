af53afb84d59d082100175cbe481840312ce52fb
nagalaxmi <nagalaxmi.m@goavega.com>
2023-03-28 18:24:14 +0530

    rendered initial three drggable plain grey widgets

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,36 @@
 <template>
-  <div id="app">
-    <div class="container">
-      <div
+  <!-- <div id="app">
+    <div class="container"> -->
+      <grid-layout :layout.sync="layout"
+              :col-num="12"
+              :row-height="30"
+              :is-draggable="draggable"
+              :is-resizable="resizable"
+              :vertical-compact="false"
+              :use-css-transforms="false"
+      >
+        <grid-item v-for="(item, index) in layout"
+                   v-bind:key=index
+                   :static="item.static"
+                   :x="item.x"
+                   :y="item.y"
+                   :w="item.w"
+                   :h="item.h"
+                   :i="item.i"
+        >
+          <span class="text">{{item.i}}</span>
+        </grid-item>
+    </grid-layout>
+      <!-- <div
         v-for="(l, index) in layout"
         v-bind:key="index"
         v-bind:class="getClass(index)"
       >
         <div :is="l.component" v-bind="getProps(index)" />
-      </div>
-    </div>
-  </div>
+      </div> -->
+
+    <!-- </div>
+  </div> -->
 </template>
 
 <script lang="ts">
@@ -23,31 +44,20 @@ import SlideshowWidget from "./components/SlideshowWidget.vue";
 import { EventSink, eventType } from "./eventsink";
 import { BaseUrl } from "./constants";
 import { dashboardStaticConfig } from "./dashboard-static-config";
+import { GridLayout, GridItem } from "vue-grid-layout";
 
 type layoutType = {
-  component: string;
-  class: string | string[];
-  props: Record<string, unknown>;
-  state?: Record<string, unknown>;
+  x: number;
+  y: number;
+  w: number;
+  h?: number;
+  i: string;
 };
 interface Classes {
   column: boolean;
   [key: string]: boolean;
 }
-const getLayout = async () => {
-  const url = window.location.pathname;
-  const path = url.substring(url.lastIndexOf("/") + 1);
-  // const response = await fetch(`${BaseUrl}api/layout`, {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-type": "application/json"
-  //   },
-  //   body: JSON.stringify({ path })
-  // });
-  const response = dashboardStaticConfig['dashboards'][0]['widgets'];
-  // console.log("🚀 ~ file: App.vue:49 ~ getLayout ~ response:", response.json());
-  return response;
-};
+
 @Component({
   components: {
     Clock,
@@ -55,7 +65,9 @@ const getLayout = async () => {
     ListWidget,
     ComparisonWidget,
     WeatherWidget,
-    SlideshowWidget
+    SlideshowWidget,
+    GridLayout,
+    GridItem
   }
 })
 export default class App extends Vue {
@@ -74,19 +86,20 @@ export default class App extends Vue {
   mounted() {
     (async () => {
       // get the layout, for now we get it from local
-      this.layout = await getLayout();
+      // this.layout = layout;
+
       console.log("🚀 ~ file: App.vue:79 ~ App ~ this.layout:", this.layout);
       // set the Vuex state modules
-      this.layout.forEach(layoutItem => {
-        if (!layoutItem.state) {
-          return;
-        }
-        const moduleState = {
-          state: () => ({ data: {} })
-        };
-        const stateKey = Object.keys(layoutItem.state)[0];
-        this.$store.registerModule(stateKey, moduleState);
-      });
+      // this.layout.forEach(layoutItem => {
+      //   if (!layoutItem.state) {
+      //     return;
+      //   }
+      //   const moduleState = {
+      //     state: () => ({ data: {} })
+      //   };
+      //   const stateKey = Object.keys(layoutItem.state)[0];
+      //   this.$store.registerModule(stateKey, moduleState);
+      // });
     })();
     (async () => {
       await this.eventServer.init((message: eventType<object>) =>
@@ -94,21 +107,33 @@ export default class App extends Vue {
       );
     })();
   }
-  getClass(index: number): { column: boolean } & Record<string, boolean> {
-    const classes: Classes = {
-      column: true
-    };
-    const classNames = Array.isArray(this.layout[index].class)
-      ? this.layout[index].class
-      : [this.layout[index].class];
-    (classNames as string[]).forEach(cls => {
-      classes[cls] = true;
-    });
-    return classes;
+  data() {
+    return {
+        layout: [
+            {"x":0,"y":0,"w":2,"h":2,"i":"bsGenerator", static: false},
+            {"x":2,"y":0,"w":2,"h":4,"i":"jotd", static: false},
+            {"x":4,"y":0,"w":2,"h":5,"i":"clock", static: false}
+        ],
+        draggable: true,
+        resizable: true,
+        index: 0
+    }
   }
-  getProps(index: number): Record<string, unknown> {
-    return this.layout[index].props;
-  }
+  // getClass(index: number): { column: boolean } & Record<string, boolean> {
+  //   const classes: Classes = {
+  //     column: false
+  //   };
+  //   const classNames = Array.isArray(this.layout[index].class)
+  //     ? this.layout[index].class
+  //     : [this.layout[index].class];
+  //   (classNames as string[]).forEach(cls => {
+  //     classes[cls] = false;
+  //   });
+  //   return classes;
+  // }
+  // getProps(index: number): Record<string, unknown> {
+  //   return this.layout[index].props;
+  // }
   beforeDestroy() {
     this.eventServer.destory();
   }
@@ -116,60 +141,109 @@ export default class App extends Vue {
 </script>
 
 <style lang="scss">
-body {
-  margin: 0;
-  padding: 0;
-  background: #333;
-  color: #efefef;
-  font-family: Karla, sans-serif;
-}
+// body {
+//   margin: 0;
+//   padding: 0;
+//   background: #333;
+//   color: #efefef;
+//   font-family: Karla, sans-serif;
+// }
 
-body * {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-size: 15px;
-}
+// body * {
+//   margin: 0;
+//   padding: 0;
+//   box-sizing: border-box;
+//   font-size: 15px;
+// }
 
-$base: 20;
+// $base: 20;
 
-@for $i from 1 through 4 {
-  h#{$i} {
-    font-size: $base + (4-$i) * 10px;
-  }
-}
+// @for $i from 1 through 4 {
+//   h#{$i} {
+//     font-size: $base + (4-$i) * 10px;
+//   }
+// }
 
-.container {
-  height: 100vh;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
-  gap: 4px;
-  width: 100%;
-  grid-auto-rows: 1fr;
-}
+// .container {
+//   height: 100vh;
+//   display: grid;
+//   grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+//   gap: 4px;
+//   width: 100%;
+//   grid-auto-rows: 1fr;
+// }
 
-.column {
-  @for $i from 1 through 4 {
-    &.x#{$i} {
-      grid-column-end: span $i;
-    }
-    &.y#{$i} {
-      grid-row-end: span $i;
-    }
-  }
-  & > div {
-    height: 100%;
-  }
+// .column {
+//   @for $i from 1 through 4 {
+//     &.x#{$i} {
+//       grid-column-end: span $i;
+//     }
+//     &.y#{$i} {
+//       grid-row-end: span $i;
+//     }
+//   }
+//   & > div {
+//     height: 100%;
+//   }
+// }
+// .text-center {
+//   text-align: center;
+// }
+// .center {
+//   display: flex;
+//   align-items: center;
+//   > div {
+//     text-align: center;
+//     width: 100%;
+//   }
+// }
+.vue-grid-layout {
+    background: #eee;
 }
-.text-center {
-  text-align: center;
+.vue-grid-item:not(.vue-grid-placeholder) {
+    background: #ccc;
+    border: 1px solid black;
 }
-.center {
-  display: flex;
-  align-items: center;
-  > div {
+.vue-grid-item .resizing {
+    opacity: 0.9;
+}
+.vue-grid-item .static {
+    background: #cce;
+}
+.vue-grid-item .text {
+    font-size: 24px;
     text-align: center;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+    height: 100%;
     width: 100%;
-  }
+}
+.vue-grid-item .no-drag {
+    height: 100%;
+    width: 100%;
+}
+.vue-grid-item .minMax {
+    font-size: 12px;
+}
+.vue-grid-item .add {
+    cursor: pointer;
+}
+.vue-draggable-handle {
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    top: 0;
+    left: 0;
+    background: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='10' height='10'><circle cx='5' cy='5' r='5' fill='#999999'/></svg>") no-repeat;
+    background-position: bottom right;
+    padding: 0 8px 8px 0;
+    background-repeat: no-repeat;
+    background-origin: content-box;
+    box-sizing: border-box;
+    cursor: pointer;
 }
 </style>
